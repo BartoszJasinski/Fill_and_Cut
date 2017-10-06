d5130350450a655f7688618392be24611edb1c12
nemoqwe08 <bjasinski8+github@gmail.com>
2017-10-06 23:48:13 +0200

    Drawing sample polygons works

---
+++ Sketchpad/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sketchpad
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawIt();
+        }
+
+        private void DrawIt()
+        {
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
+                50, 100, 150, 150);
+            graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
+            graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+        }
+
+  
+    }
+}
--- Sketchpad/Program.cs
+++ Sketchpad/Program.cs
@@ -16,7 +16,7 @@ namespace Sketchpad
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
