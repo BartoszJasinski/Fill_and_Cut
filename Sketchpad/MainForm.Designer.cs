d1bffc566a487a17db52132f694e90b19559b2d8
nemoqwe08 <bjasinski8+github@gmail.com>
2017-10-08 13:15:28 +0200

    Adding and dragging vertices works (with bug durring dragging)

--- Sketchpad/MainForm.Designer.cs
+++ Sketchpad/MainForm.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
-            // button1
+            // canvas
             // 
-            this.button1.Location = new System.Drawing.Point(37, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(622, 524);
+            this.canvas.TabIndex = 3;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            this.canvas.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDoubleClick);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 524);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
             this.Name = "MainForm";
             this.Text = "SketchPad";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -55,8 +59,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel canvas;
     }
 }
 
--- Sketchpad/MainForm.cs
+++ Sketchpad/MainForm.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Shapes;
 
 namespace Sketchpad
 {
     public partial class MainForm : Form
     {
+        private List<Point> polygon = new List<Point>();
+        private Size vertexSize = new Size(3, 3);
+        private Brush vertexColor = Brushes.Red, lineColor = Brushes.LightGreen, boundingBoxColor = Brushes.LightBlue;
+        private int spaceBetweenPolygonAndBoundingBox = 10;
+        private Point? _mouseLocation;
+
+        private bool changeVertexPosition = false; //REIMPLEMENT
+        Point draggedVertexIndex;
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,23 +29,164 @@ namespace Sketchpad
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            //this.canvas.MouseDown += this.canvas_MouseDown;
+            //this.canvas.MouseUp += this.canvas_MouseUp;
+            //this.canvas.MouseMove += this.canvas_MouseMove;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private Point MouseDownLocation;
+
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
         {
-            DrawIt();
+            Graphics graphics = canvas.CreateGraphics();
+            Point clickCoordinates = new Point(e.X, e.Y), vertexCoordinates = new Point(e.X, e.Y);
+
+            HandleClick(graphics, polygon, clickCoordinates);
+            
+            graphics.Dispose();
+          
         }
 
-        private void DrawIt()
+        //private void canvas_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (e.Button == System.Windows.Forms.MouseButtons.Left)
+        //    {
+        //        ChangeVertexPosition(vertice, e.Location);
+
+        //    }
+        //}
+
+        void canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            System.Drawing.Graphics graphics = this.CreateGraphics();
-            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
-                50, 100, 150, 150);
-            graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
-            graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+            if (!changeVertexPosition)
+                return;
+            Graphics graphics = canvas.CreateGraphics();
+
+            draggedVertexIndex = FindIfClickedNearVertex(e.Location);
+            ChangeVertexPosition(draggedVertexIndex, e.Location);
+            PaintCanvas(graphics, polygon);
+            //Invalidate();
+
+            graphics.Dispose();
         }
 
-  
+        private void ChangeVertexPosition(Point vertice, Point newVertice)
+        {
+            for(int i = 0; i < polygon.Count; i++)
+            {
+                if (polygon[i].Equals(vertice))
+                    polygon[i] = newVertice;
+            }
+            //foreach(var point in polygon.Where(p => p.X == vertice.X/* && p.Y == vertice.Y */))
+            //{
+            //    point.X = newVertice.X;
+            //    point.Y = newVertice.Y;
+            //}
+            //List<Point> tmp_polygon = polygon.Where(p => p.X == vertice.X/* && p.Y == vertice.Y */).ToList();
+
+            //    tmp_polygon.ForEach(p => /*{*/ p.X = newVertice.X/*; p.Y = newVertice.Y; }*/);
+        }
+
+        void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            //this._mouseLocation = e.Location;
+            changeVertexPosition = false;
+        }
+
+        //void canvas_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    //Check if you've left-clicked if you want
+        //    this._mouseLocation = e.Location;
+        //}
+
+        private void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+         
+        }
+
+        private void PaintCanvas(Graphics graphics, List<Point> polygon)
+        {
+            graphics.Clear(Color.White);
+
+            DrawPolygon(graphics, polygon);
+            DrawBoundingBox(graphics, polygon);
+        }
+
+        private void HandleClick(Graphics graphics, List<Point> polygon, Point clickCoordinates)
+        {
+            if ((draggedVertexIndex = FindIfClickedNearVertex(clickCoordinates)).Equals(new Point(-1, -1)))
+            {
+                AddVerticeToPolygon(polygon, clickCoordinates);
+                PaintCanvas(graphics, polygon);
+            }
+            else
+                changeVertexPosition = true;
+
+        }
+
+        private void DrawBoundingBox(Graphics graphics, List<Point> polygon)
+        {
+            int maxX = polygon.Max(p => p.X), maxY = polygon.Max(p => p.Y), minX = polygon.Min(p => p.X), minY = polygon.Min(p => p.Y);
+            System.Drawing.Rectangle boundingBox = new System.Drawing.Rectangle(minX - spaceBetweenPolygonAndBoundingBox / 2, minY - spaceBetweenPolygonAndBoundingBox / 2,
+                maxX - minX + spaceBetweenPolygonAndBoundingBox, maxY - minY + spaceBetweenPolygonAndBoundingBox);
+            graphics.DrawRectangle(new Pen(boundingBoxColor), boundingBox);
+
+        }
+
+        private void AddVerticeToPolygon(List<Point> polygon, Point clickCoordinates)
+        {
+            if (!polygon.Any((Point p) => { return p.Equals(clickCoordinates); }))
+                polygon.Add(clickCoordinates);
+        }
+
+        private Point FindIfClickedNearVertex(Point clickCoordinates)
+        {
+            // what if at least two vertices are <= selectVertexAreaSize
+            int selectVertexAreaSize = 5; // variable to store size of area if clicked to select vertex in that area 
+            List<Point> verticesNearClick = polygon.FindAll((Point p) => { return Math.Abs(p.X - clickCoordinates.X) <= selectVertexAreaSize && Math.Abs(p.Y - clickCoordinates.Y) <= selectVertexAreaSize; });
+            int indexForMinValue = 0;
+            //Searches for vertice which is nearest to click
+            for (int i = 0; i < verticesNearClick.Count; i++)
+            {
+                if (verticesNearClick[indexForMinValue].X - clickCoordinates.X + verticesNearClick[indexForMinValue].Y - clickCoordinates.Y >
+                    verticesNearClick[i].X - clickCoordinates.X + verticesNearClick[i].Y - clickCoordinates.Y)
+                    indexForMinValue = i;
+            }
+            if (verticesNearClick.Count > 0)
+                return verticesNearClick[indexForMinValue];
+            else
+                return new Point(-1, -1);
+        }
+
+
+        private void DrawPolygon(Graphics graphics, List<Point> polygon)
+        {
+            
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                DrawVertex(graphics, polygon[i]);
+                MyDrawLine(graphics, polygon[i], polygon[(i + 1) % polygon.Count]);
+            }
+        }
+
+        private void DrawVertex(Graphics graphics, Point vertexCoordinates)
+        {
+            System.Drawing.Rectangle vertex = new System.Drawing.Rectangle(vertexCoordinates, vertexSize);
+            graphics.FillRectangle(vertexColor, vertex);
+        }
+
+        private void canvas_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            FindIfClickedNearVertex(new Point(e.X, e.Y));
+        }
+
+        //TODO Reimpelement with custom Bersenham algorithm
+        private void MyDrawLine(Graphics graphics, Point p1, Point p2)
+        {
+            graphics.DrawLine(new Pen(lineColor), p1, p2);
+        }
+
     }
 }
